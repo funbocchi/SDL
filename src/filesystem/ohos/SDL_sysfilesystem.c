63a43c479faccdee3f871ae098585ab7ba09559f
funbocchi <lzx13735999188@petalmail.com>
2026-09-22 04:33:11 +0800

    feat(ohos): implement filesystem directory return and refine variable naming - Refine variable naming in SDL_ohos.c and SDL_sysfilesystem.c for better readability. - Retrieve and cache the resource directory during initialization. - Implement SDL_sysfilesystem directory return based on the cached resource directory.

--- src/filesystem/ohos/SDL_sysfilesystem.c
+++ src/filesystem/ohos/SDL_sysfilesystem.c
@@ -1,10 +1,67 @@
+/*
+  Simple DirectMedia Layer
+  Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>
 
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
 
-#include "SDL_internal.h"
-#include "SDL_error.h"
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
+
+#include "../../SDL_internal.h"
+
+#ifdef SDL_FILESYSTEM_OHOS
+
 #include "SDL_filesystem.h"
+#include "SDL_stdinc.h"
 
-char*SDL_GetBasePath(void){
-    SDL_Unsupported();
-    return NULL;
+/**
+* @brief 在初始化时获取并完成对两个路径的初始化，具体代码通过 Napi 调用
+*/
+static char *s_base_path = NULL;
+static char *s_pref_path = NULL;
+
+void OHOS_SetBasePath(const char *path)
+{
+    if (s_base_path) {
+        SDL_free(s_base_path);
+    }
+    s_base_path = SDL_strdup(path);
 }
+
+void OHOS_SetPrefPath(const char *path)
+{
+    if (s_pref_path) {
+        SDL_free(s_pref_path);
+    }
+    s_pref_path = SDL_strdup(path);
+}
+
+char *SDLCALL SDL_GetBasePath(void)
+{
+    if(s_base_path==NULL){
+        return NULL;
+    }
+    return SDL_strdup(s_base_path);
+}
+
+char *SDLCALL SDL_GetPrefPath(const char *org, const char *app)
+{
+    if(s_pref_path == NULL){
+        return NULL;
+    }
+    return SDL_strdup(s_pref_path);
+}
+
+#endif
